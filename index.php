4d3142756ddc4fc3d49a874edbedd06e461199bb
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-03 17:34:58 +0200

    rifinito

--- index.php
+++ index.php
@@ -13,17 +13,30 @@
     Visualizzare a schermo il paragrafo con la
     relative lunghezza e sostituire la
     badword passata in GET con tre *. -->
-    <h1>ciao</h1>
+    <div>
+    <h1>Versione originale</h1>
     <?php
-     $name ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem natus voluptates cumque praesentium vel blanditiis omnis modi repudiandae dolore? Omnis optio odio eaque fuga praesentium soluta, dignissimos ut impedit deserunt.';
-     echo $_GET['value'];
-    //  echo $name;
-     $realName1 = explode(' ',$name);
-     $realName = str_replace('Lorem','  ***  ',$name);
-     $lunghezza = strlen($name);
-     echo $realName;
-     echo $realName1;
-     echo $lunghezza;
+    $name ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem natus voluptates cumque praesentium vel blanditiis omnis modi repudiandae dolore? Omnis optio odio eaque fuga praesentium soluta, dignissimos ut impedit deserunt.';
+    echo $name;
     ?>
+    </div>
+    <div>
+    <h1>Versione tradotta</h1>
+
+    <?php
+    $realName = str_replace('Lorem','  ***  ',$name);
+    echo $realName;
+    ?>
+    </div>
+    <div>
+    <h1>lunghezza</h1>
+    <?php
+    $lunghezza = strlen($name);
+    echo $lunghezza;
+    ?>
+    </div>
+
+
+
 </body>
 </html>
